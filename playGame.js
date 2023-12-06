eea0b4c33972e899394e79aa285d700e2695b7f2
bondogji <Osamabondogji@gmail.com>
2023-12-05 20:24:55 -0500

    finished index fi

--- playGame.js
+++ playGame.js
@@ -1,8 +1,6 @@
 class playGame extends Phaser.Scene {
   constructor() {
     super("PlayGame");
-    this.availableBackgrounds = ["bg_1", "bg_2", "bg_3"]; // Add more background keys as needed
-    this.currentBackgroundIndex = 0;
   }
   create() {
     // create an tiled sprite with the size of our game screen
@@ -38,59 +36,36 @@ class playGame extends Phaser.Scene {
 
     // allow key inputs to control the player
     this.cursors = this.input.keyboard.createCursorKeys();
-    this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE); // Add SPACE key
 
 
-     // set world bounds to allow camera to follow the player
+    // set workd bounds to allow camera to follow the player
     this.myCam = this.cameras.main;
     this.myCam.setBounds(0, 0, game.config.width * 3, game.config.height);
 
     // making the camera follow the player
     this.myCam.startFollow(this.player);
 
-   
-
   }
 
 
-    update() {
+  update() {
+
     // move the player when the arrow keys are pressed
     if (this.cursors.left.isDown && this.player.x > 0) {
       this.player.x -= 3;
       this.player.scaleX = 1;
+
     } else if (this.cursors.right.isDown && this.player.x < game.config.width * 3) {
       this.player.x += 3;
       this.player.scaleX = -1;
-    }
 
-    // move the player up when the up arrow key is pressed
-    if (this.cursors.up.isDown && this.player.y > 0) {
-      this.player.y -= 3;
     }
-
-    // move the player down when the down arrow key is pressed
-    if (this.cursors.down.isDown && this.player.y < game.config.height) {
-      this.player.y += 3;
-    }
-    // Check if the SPACE key is pressed to cycle through backgrounds
-    if (Phaser.Input.Keyboard.JustDown(this.spacebar)) {
-      this.changeBackground();
-    }
-
 
     // scroll the texture of the tilesprites proportionally to the camera scroll
-    this.bg_1.tilePositionX = this.myCam.scrollX * 0.3;
-    this.bg_2.tilePositionX = this.myCam.scrollX * 0.6;
+    this.bg_1.tilePositionX = this.myCam.scrollX * .3;
+    this.bg_2.tilePositionX = this.myCam.scrollX * .6;
     this.ground.tilePositionX = this.myCam.scrollX;
-  }
 
-  changeBackground() {
-    // Cycle through the available backgrounds
-    this.currentBackgroundIndex = (this.currentBackgroundIndex + 1) % this.availableBackgrounds.length;
-    
-    // Set the current background
-    const currentBackgroundKey = this.availableBackgrounds[this.currentBackgroundIndex];
-    this.bg_1.setTexture(currentBackgroundKey);
-    this.bg_2.setTexture(currentBackgroundKey);
+
   }
 }
